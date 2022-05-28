4e5ce7f71c77c708ce817eb45fe3b9608f0d14d4
Miruna Chi <miruna.chiricu@gmail.com>
2022-05-28 20:35:20 +0300

    Implemented task 7

--- labtasks/GoogleSearcher/app/src/main/java/ro/pub/cs/systems/eim/lab07/googlesearcher/network/GoogleSearcherAsyncTask.java
+++ labtasks/GoogleSearcher/app/src/main/java/ro/pub/cs/systems/eim/lab07/googlesearcher/network/GoogleSearcherAsyncTask.java
@@ -1,7 +1,15 @@
 package ro.pub.cs.systems.eim.lab07.googlesearcher.network;
 
 import android.os.AsyncTask;
+import android.util.Log;
 import android.webkit.WebView;
+
+import cz.msebera.android.httpclient.client.HttpClient;
+import cz.msebera.android.httpclient.client.ResponseHandler;
+import cz.msebera.android.httpclient.client.methods.HttpGet;
+import cz.msebera.android.httpclient.impl.client.BasicResponseHandler;
+import cz.msebera.android.httpclient.impl.client.DefaultHttpClient;
+import ro.pub.cs.systems.eim.lab07.googlesearcher.general.Constants;
 
 public class GoogleSearcherAsyncTask extends AsyncTask<String, Void, String> {
 
@@ -14,13 +22,30 @@ public class GoogleSearcherAsyncTask extends AsyncTask<String, Void, String> {
     @Override
     protected String doInBackground(String... params) {
 
+        String result = "";
         // TODO exercise 6b)
-        // create an instance of a HttpClient object
-        // create an instance of a HttpGet object, encapsulating the base Internet address (http://www.google.com) and the keyword
-        // create an instance of a ResponseHandler object
-        // execute the request, thus generating the result
+        try {
 
-        return null;
+            // create an instance of a HttpClient object
+            HttpClient httpClient = new DefaultHttpClient();
+
+            // create an instance of a HttpGet object, encapsulating the base Internet address (http://www.google.com) and the keyword
+            HttpGet httpGet = new HttpGet(Constants.GOOGLE_INTERNET_ADDRESS + params[0]);
+
+            // create an instance of a ResponseHandler object
+            ResponseHandler<String> responseHandler = new BasicResponseHandler();
+
+            // execute the request, thus generating the result
+            result = httpClient.execute(httpGet, responseHandler);
+
+        } catch (Exception exception) {
+            Log.e(Constants.TAG, exception.getMessage());
+            if (Constants.DEBUG) {
+                exception.printStackTrace();
+            }
+        }
+
+        return result;
     }
 
     @Override
@@ -28,6 +53,8 @@ public class GoogleSearcherAsyncTask extends AsyncTask<String, Void, String> {
 
         // TODO exercise 6b)
         // display the result into the googleResultsWebView through loadDataWithBaseURL() method
+        googleResultsWebView.loadDataWithBaseURL("http://www.google.com", content, "text/html", "UTF-8", null);
+
         // - base Internet address is http://www.google.com
         // - page source code is the response
         // - mimetype is text/html
--- labtasks/GoogleSearcher/app/src/main/java/ro/pub/cs/systems/eim/lab07/googlesearcher/view/GoogleSearcherActivity.java
+++ labtasks/GoogleSearcher/app/src/main/java/ro/pub/cs/systems/eim/lab07/googlesearcher/view/GoogleSearcherActivity.java
@@ -4,11 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import ro.pub.cs.systems.eim.lab07.googlesearcher.R;
+import ro.pub.cs.systems.eim.lab07.googlesearcher.general.Constants;
+import ro.pub.cs.systems.eim.lab07.googlesearcher.network.GoogleSearcherAsyncTask;
 
 public class GoogleSearcherActivity extends AppCompatActivity {
 
@@ -23,10 +26,24 @@ public class GoogleSearcherActivity extends AppCompatActivity {
         public void onClick(View view) {
             // TODO exercise 6a)
             // obtain the keyword from keywordEditText
+            String key_sentence = keywordEditText.getText().toString();
+
             // signal an empty keyword through an error message displayed in a Toast window
+            if (key_sentence == null || key_sentence.isEmpty())
+                Toast.makeText(getApplicationContext(), Constants.EMPTY_KEYWORD_ERROR_MESSAGE, Toast.LENGTH_LONG).show();
+
             // split a multiple word (separated by space) keyword and link them through +
             // prepend the keyword with "search?q=" string
+            String[] arrOfStr = key_sentence.split(" ");
+            String search_format = Constants.SEARCH_PREFIX;
+
+            for (String a : arrOfStr) {
+                search_format += a + "+";
+            }
+            search_format = search_format.substring(0, search_format.length() - 1);
+
             // start the GoogleSearcherAsyncTask passing the keyword
+            new GoogleSearcherAsyncTask(googleResultsWebView).execute(search_format);
         }
     }
 
@@ -37,6 +54,13 @@ public class GoogleSearcherActivity extends AppCompatActivity {
 
         keywordEditText = (EditText)findViewById(R.id.keyword_edit_text);
         googleResultsWebView = (WebView)findViewById(R.id.google_results_web_view);
+
+        googleResultsWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return false;
+            }
+        });
 
         searchGoogleButton = (Button)findViewById(R.id.search_google_button);
         searchGoogleButton.setOnClickListener(searchGoogleButtonClickListener);
